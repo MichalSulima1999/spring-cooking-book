4152525c6345308992c5771582836b0a141de95d
Michael1099 <michal.sulima1999@gmail.com>
2022-11-20 18:10:54 +0100

    Added Cors mapping

---
+++ recipe-book-backend/src/main/java/com/example/recipebook/configs/WebConfig.java
@@ -0,0 +1,19 @@
+package com.example.recipebook.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
