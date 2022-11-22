4e4e4ec2952356d94193a85e579c8f1e05315268
MichalSulima1999 <michal.sulima1999@gmail.com>
2022-11-22 21:08:20 +0100

    Added home route

--- recipe-book-frontend/src/components/Pages.jsx
+++ recipe-book-frontend/src/components/Pages.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import AddRecipe from "./recipes/AddRecipe";
 import Recipes from "./recipes/Recipes";
+import Home from "./home/Home";
 
 function Pages() {
   const location = useLocation();
   return (
     <Routes location={location} key={location.pathname}>
+      <Route path="/" element={<Home />} />
       <Route path="/recipes" element={<Recipes />} />
       <Route path="/recipes/add" element={<AddRecipe />} />
     </Routes>
