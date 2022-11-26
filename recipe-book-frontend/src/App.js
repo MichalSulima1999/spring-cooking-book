1c51aae3e75f7a65fe7411f67d5cf227c62e2d23
ArturSulej <sulejartur2@gmail.com>
2022-11-26 18:22:04 +0100

    Drobne zmiany w wyglądzie. Dodano lokalizację PL

--- recipe-book-frontend/src/App.js
+++ recipe-book-frontend/src/App.js
@@ -2,6 +2,7 @@ import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import Pages from "./components/Pages";
 
+
 function App() {
   return (
     <div className="d-flex flex-column min-vh-100">
