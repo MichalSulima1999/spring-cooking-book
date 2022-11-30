9c0c6215a0e2eac26caefece40a2bf42f91749a4
MichalSulima1999 <Michal.sulima1999@gmail.com>
2022-11-30 09:59:36 +0100

    Created controller production code for adding categories

--- recipe-book-backend/src/main/java/com/example/recipebook/category/CategoryController.java
+++ recipe-book-backend/src/main/java/com/example/recipebook/category/CategoryController.java
@@ -3,11 +3,10 @@ package com.example.recipebook.category;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -24,5 +23,12 @@ public record CategoryController(CategoryService categoryService) {
     public ResponseEntity<List<Category>> getLimitedCategoriesByDescription(@Parameter(description = "Category name like")
                                                                             @RequestParam(name = "name") String categoryName) {
         return ResponseEntity.ok().body(categoryService.getLimitedCategoriesByName(categoryName));
+    }
+
+    @Operation(summary = "Add category")
+    @PostMapping
+    public ResponseEntity<Category> addCategory(@RequestBody Category category) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/category").toUriString());
+        return ResponseEntity.created(uri).body(categoryService.addCategory(category));
     }
 }
