893d5bc86ad3dc1b4796dcdc51c4c1415fa18359
MichalSulima1999 <michal.sulima1999@gmail.com>
2022-12-06 16:15:19 +0100

    Added new allowed origin to CORS

--- recipe-book-backend/src/main/java/com/example/recipebook/configs/WebConfig.java
+++ recipe-book-backend/src/main/java/com/example/recipebook/configs/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://localhost:4200")
                 .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
